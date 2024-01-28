168c02d2828a5110255906d3173e99ee66599442
Deyvid Tavares <devdeyvid2022@gmail.com>
2024-01-28 10:11:12 -0300

    feat: aside link selected

--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -1,25 +1,30 @@
+"use client"
+
 import Image from "next/image";
 import { details } from "../../details";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 export function Aside() {
+
+    const path = useParams().id
     return (
         <aside className="w-1/5 shadow-md px-8 h-full pt-16 bg-white hidden md:flex text-primary flex-col items-center">
             <Image src={details.image} alt="logo" className="mt-4" width={150} height={100} />
             <ul className="flex flex-col gap-4 text-primary mt-8">
-                <li className="">
+                <li className={path === "schedules" ? "text-primary font-medium underline" : ""}>
                     <Link href="/dashboard/schedules">Agenda</Link>
                 </li>
-                <li>
+                <li className={path === "pending" ? "text-primary font-medium underline" : ""}>
                     <Link href="/dashboard/pending">Pendentes</Link>
                 </li>
-                <li>
+                <li className={path === "patients" ? "text-primary font-medium underline" : ""}>
                     <Link href="/dashboard/patients">Pacientes</Link>
                 </li>
-                <li>
+                <li className={path === "professionals" ? "text-primary font-medium underline" : ""}>
                     <Link href="/dashboard/professionals">Profissionais</Link>
                 </li>
-                <li>
+                <li className={path === "availability" ? "text-primary font-medium underline" : ""}>
                     <Link href="/dashboard/availability">Disponibilidade</Link>
                 </li>
             </ul>
