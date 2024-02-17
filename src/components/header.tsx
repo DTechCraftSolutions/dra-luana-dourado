1ef58635ed1f0aa29b9fd05c881a22c0a16f9a62
Deyvid Tavares <devdeyvid2022@gmail.com>
2024-02-17 12:52:07 -0300

    feat: user popover

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,15 @@
 import { IoMdNotifications } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import Image from "next/image";
+
 
 
 
@@ -13,12 +23,28 @@ export function Header() {
                 <button className="w-8 h-8 flex items-center justify-center">
                     <IoMdNotifications />
                 </button>
-                <button className="flex items-center justify-center">
-                    <p>
-                        Usuário
-                    </p>
-                    <IoIosArrowDown />
-                </button>
+                <Popover>
+                    <PopoverTrigger className="flex items-center justify-center">
+                        <p>
+                            Usuário
+                        </p>
+                        <IoIosArrowDown />
+                    </PopoverTrigger>
+                    <PopoverContent className=" flex flex-col gap-4">
+                        <Image src="/dtr-luana-logo.png" alt="logo" width={100} height={100} />
+                        <p>
+                            <span className="font-semibold text-primary mr-1">
+                                Usuário
+                            </span>
+                            Seja Bem vindo &#40;a&#41; ao painel de controle Dra Luana dourado
+                        </p>
+
+                        <Button variant="outline" className="w-full">
+                            Sair
+                        </Button>
+                    </PopoverContent>
+
+                </Popover>
             </div>
         </header>
     )
