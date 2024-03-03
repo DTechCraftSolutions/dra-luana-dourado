9a4cadb620bb4ddd48a93e4ea9a7bd209a81bf52
Deyvid Tavares <devdeyvid2022@gmail.com>
2024-03-03 16:22:59 -0300

    feat: finish prescription

---
+++ src/data/medicine.ts
@@ -0,0 +1,142 @@
+export const Medicines = [
+    {
+        "nome": "Codeína",
+        "posologia": "10 mg a cada 4 horas, conforme necessário para a dor."
+    },
+    {
+        "nome": "Tramadol",
+        "posologia": "50 mg a cada 6 horas, conforme necessário para a dor moderada a intensa."
+    },
+    {
+        "nome": "Alprazolam",
+        "posologia": "0,25 mg a 0,5 mg a cada 8 horas, conforme necessário para ansiedade."
+    },
+    {
+        "nome": "Bromazepam",
+        "posologia": "3 mg a cada 8 horas, conforme necessário para ansiedade."
+    },
+    {
+        "nome": "Diazepam",
+        "posologia": "2 mg a 5 mg a cada 8 horas, conforme necessário para ansiedade."
+    },
+    {
+        "nome": "Amitriptilina",
+        "posologia": "25 mg a 50 mg uma vez ao dia, antes de dormir para depressão."
+    },
+    {
+        "nome": "Imipramina",
+        "posologia": "25 mg a 50 mg uma vez ao dia, antes de dormir para depressão."
+    },
+    {
+        "nome": "Desipramina",
+        "posologia": "25 mg a 50 mg uma vez ao dia, antes de dormir para depressão."
+    },
+    {
+        "nome": "Celecoxibe",
+        "posologia": "200 mg duas vezes ao dia, com ou sem alimentos para a dor."
+    },
+    {
+        "nome": "Valdecoxibe",
+        "posologia": "10 mg uma vez ao dia, com ou sem alimentos para a dor."
+    },
+    {
+        "nome": "Dipirona",
+        "posologia": "500 mg a cada 6 horas, conforme necessário para a dor."
+    },
+    {
+        "nome": "Diclofenaco",
+        "posologia": "50 mg a cada 8 horas, conforme necessário para a dor."
+    },
+    {
+        "nome": "Tramadol",
+        "posologia": "50 mg a cada 6 horas, conforme necessário para a dor moderada a intensa."
+    },
+    {
+        "nome": "Cloridrato de Lidocaína",
+        "posologia": "Aplicar topicamente na área afetada até 3 vezes ao dia."
+    },
+    {
+        "nome": "Amoxicilina + Ácido Clavulânico",
+        "posologia": "875 mg a cada 12 horas, por 7 a 10 dias para infecções."
+    },
+    {
+        "nome": "Clorexidina",
+        "posologia": "Enxaguar com 10 ml duas vezes ao dia por 30 segundos."
+    },
+    {
+        "nome": "Nistatina",
+        "posologia": "Aplicar na mucosa oral 4 vezes ao dia por 7 a 14 dias."
+    },
+    {
+        "nome": "Miconazol",
+        "posologia": "Aplicar topicamente na área afetada duas vezes ao dia."
+    },
+    {
+        "nome": "Cetoprofeno",
+        "posologia": "50 mg a cada 8 horas, conforme necessário para a dor."
+    },
+    {
+        "nome": "Dexametasona",
+        "posologia": "0,5 mg a cada 6 horas, por até 2 semanas para inflamações."
+    },
+    {
+        "nome": "Metronidazol",
+        "posologia": "500 mg a cada 8 horas, por 7 dias para infecções."
+    },
+    {
+        "nome": "Naproxeno",
+        "posologia": "250 mg a cada 12 horas, conforme necessário para a dor."
+    },
+    {
+        "nome": "Hidroxicloroquina",
+        "posologia": "400 mg uma vez ao dia por 5 dias para algumas condições."
+    },
+    {
+        "nome": "Amoxicilina + Clavulanato de Potássio",
+        "posologia": "875 mg a cada 12 horas, por 7 a 10 dias para infecções."
+    },
+    {
+        "nome": "Dexametasona tópica",
+        "posologia": "Aplicar uma fina camada na área afetada até 3 vezes ao dia."
+    },
+    {
+        "nome": "Nitrato de Prata",
+        "posologia": "Aplicar topicamente conforme necessário para úlceras bucais."
+    },
+    {
+        "nome": "Ibuprofeno",
+        "posologia": "400 mg a cada 6 horas, conforme necessário para a dor."
+    },
+    {
+        "nome": "Paracetamol",
+        "posologia": "500 mg a cada 4 horas, conforme necessário para a dor."
+    },
+    {
+        "nome": "Amoxicilina",
+        "posologia": "500 mg a cada 8 horas, por 7 a 10 dias para infecções."
+    },
+    {
+        "nome": "Ciprofloxacino",
+        "posologia": "500 mg a cada 12 horas, por 7 a 14 dias para infecções."
+    },
+    {
+        "nome": "Clorhexidina",
+        "posologia": "Enxaguar com 15 ml duas vezes ao dia por 30 segundos."
+    },
+    {
+        "nome": "Fluoreto de Sódio",
+        "posologia": "Utilizar colutório diariamente conforme orientação."
+    },
+    {
+        "nome": "Ibuprofeno tópico",
+        "posologia": "Aplicar topicamente na área afetada até 4 vezes ao dia."
+    },
+    {
+        "nome": "Benzocaína",
+        "posologia": "Aplicar topicamente na área afetada conforme necessário."
+    },
+    {
+        "nome": "Amoxicilina + Ácido Clavulânico",
+        "posologia": "875 mg a cada 12 horas, por 7 a 10 dias para infecções."
+    }
+]
--- src/forms/prescription.tsx
+++ src/forms/prescription.tsx
@@ -1,10 +1,71 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { useState } from "react";
-import { IoAdd, IoArrowBack } from "react-icons/io5";
+import { FormEvent, useEffect, useRef, useState } from "react";
+import { IoAdd, IoArrowBack, IoClose, IoPrint } from "react-icons/io5";
+import { Medicines } from "@/data/medicine";
+import Autosuggest from "react-autosuggest"
+import Image from "next/image";
+import { details } from "../../details";
+import { format, set } from "date-fns";
+
+interface Medicine {
+    nome: string;
+    posologia: string;
+}
 
 export function Prescription({ setDocSelected }: any) {
     const [newPrescription, setNewPrescription] = useState(false);
     const [measureInForm, setMeasureInForm] = useState<string>("Caixa(s)");
+    const [inputValue, setInputValue] = useState('');
+    const [suggestions, setSuggestions] = useState<Medicine[]>([]);
+    const [selectedMedicine, setSelectedMedicine] = useState<Medicine | null>(null);
+    const [medicineList, setMedicineList] = useState<any[]>([]);
+    const [quantityToAdd, setQuantityToAdd] = useState(1);
+
+    const inputRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        if (selectedMedicine && inputRef.current) {
+            inputRef.current.value = selectedMedicine.posologia;
+        }
+    }, [selectedMedicine]);
+
+    const onInputChange = (
+        event: React.FormEvent,
+        { newValue }: Autosuggest.ChangeEvent
+    ) => {
+        setInputValue(newValue);
+    };
+
+    const onSuggestionSelected = (
+        event: React.FormEvent,
+        { suggestion }: Autosuggest.SuggestionSelectedEventData<Medicine>
+    ) => {
+        setInputValue(suggestion.nome);
+        setSelectedMedicine(suggestion);
+    };
+
+    const getSuggestions = (value: string): Medicine[] => {
+        const inputValue = value.trim().toLowerCase();
+        const inputLength = inputValue.length;
+        return inputLength === 0
+            ? []
+            : Medicines.filter(
+                (medicine) =>
+                    medicine.nome.toLowerCase().slice(0, inputLength) === inputValue
+            );
+    };
+    const handleAdd = (e: FormEvent) => {
+        e.preventDefault();
+        if (selectedMedicine && quantityToAdd > 0 && measureInForm) {
+            setMedicineList([...medicineList, { ...selectedMedicine, quantity: quantityToAdd, measure: measureInForm }]);
+            setInputValue("");
+            setSelectedMedicine(null);
+            setQuantityToAdd(1);
+            setMeasureInForm("Caixa(s)");
+
+        }
+    }
+
     return (
         <div className="">
             <div className="flex gap-2 items-center">
@@ -27,14 +88,29 @@ export function Prescription({ setDocSelected }: any) {
                                 <label htmlFor="" className="text-primary">
                                     Nome do medicamento
                                 </label>
-                                <input type="text" placeholder="Medicamento" className="w-full h-10 px-4 rounded-full border-[1px] focus:outline-none focus:duration-500 focus:border-primary" />
+                                <Autosuggest
+                                    suggestions={suggestions}
+                                    onSuggestionsFetchRequested={({ value }) =>
+                                        setSuggestions(getSuggestions(value))
+                                    }
+                                    onSuggestionsClearRequested={() => setSuggestions([])}
+                                    getSuggestionValue={(suggestion) => suggestion.nome}
+                                    renderSuggestion={(suggestion) => <div className="bg-white p-2 cursor-pointer hover:text-white hover:bg-primary hover:opacity-60 hover:duration-500 shadow-md">{suggestion.nome}</div>}
+                                    inputProps={{
+                                        className: "w-full h-10 px-4 rounded-full border-[1px] focus:outline-none focus:duration-500 focus:border-primary",
+                                        placeholder: "Digite o nome do medicamento",
+                                        value: inputValue,
+                                        onChange: onInputChange,
+                                    }}
+                                    onSuggestionSelected={onSuggestionSelected}
+                                />
                             </div>
                             <div className="flex items-center gap-5">
                                 <div className="flex flex-col">
                                     <label htmlFor="" className="text-primary">
                                         Quantidade
                                     </label>
-                                    <input type="text" placeholder="Qtd" className="w-24 h-10 px-4 rounded-full border-[1px] focus:outline-none focus:duration-500 focus:border-primary" />
+                                    <input value={quantityToAdd} onChange={(e) => setQuantityToAdd(Number(e.target.value))} type="text" placeholder="Qtd" className="w-24 h-10 px-4 rounded-full border-[1px] focus:outline-none focus:duration-500 focus:border-primary" />
                                 </div>
                                 <div>
                                     <label className="text-primary" htmlFor="">
@@ -53,16 +129,93 @@ export function Prescription({ setDocSelected }: any) {
                                 </div>
                             </div>
                             <div>
-                                <label htmlFor="">Posologia</label> 
-                                <textarea placeholder="Adicione..." className="w-full h-32 px-3 rounded-lg border-[1px] focus:outline-none focus:duration-500 focus:border-primary" />
+                                <label htmlFor="">Posologia</label>
+                                <textarea
+                                    id="posologia"
+                                    ref={inputRef}
+                                    placeholder="Adicione..."
+                                    className="w-full h-32 px-3 rounded-lg border-[1px] focus:outline-none focus:duration-500 focus:border-primary"
+                                />
                             </div>
-                            <button type="button" className="bg-primary mt-5 px-5 flex items-center text-white rounded-full py-2 justify-center gap-2 hover:opacity-80 hover:duration-1000 hover:ease-out">
+                            <button onClick={handleAdd} className="bg-primary mt-5 px-5 flex items-center text-white rounded-full py-2 justify-center gap-2 hover:opacity-80 hover:duration-1000 hover:ease-out">
                                 <IoAdd />
                                 Adicionar
                             </button>
                         </form>
-                        <div className="w-3/5 h-[90vh] bg-white shadow-md rounded-lg mb-10">
-                            Receita
+                        <div className="w-3/5 h-[90vh] bg-white shadow-md flex justify-between flex-col rounded-lg p-2 px-8 mb-10">
+                            <div>
+                                <div className="flex items-center gap-4">
+                                    <Image src={details.image} alt="logo" className="mt-4" width={150} height={100} />
+                                    <div className="text-sm flex w-full flex-col gap-2">
+                                        <p>Nome do dentista</p>
+                                        <p className="flex gap-2">
+                                            <span className="font-bold">
+                                                CRO
+                                            </span>
+                                            7777777777
+                                        </p>
+                                    </div>
+                                    <button className="bg-primary fixed-right text-white flex items-center text-sm gap-2 px-5 py-2 rounded-full hover:opacity-80 hover:duration-1000 hover:ease-out">
+                                        <IoPrint />
+                                        Imprimir
+                                    </button>
+                                </div>
+                                <h2 className="text-center w-full font-medium">Receita</h2>
+                                <div className="text-sm mt-5">
+                                    <p>
+                                        <span className="font-bold">
+                                            Paciente
+                                        </span>: Nome do paciente
+                                    </p>
+                                    <p>
+                                        Rua da minha casa, juá, 16, Limoeiro - PE
+                                    </p>
+                                </div>
+                                {
+                                    medicineList.length > 0 && medicineList.map((medicine, index) => (
+                                        <div className="mt-5 text-sm">
+                                            <button onClick={() => {
+                                                const newList = medicineList.filter((medicine) => {
+                                                    return medicine.nome !== medicineList[index].nome
+                                                })
+                                                setMedicineList(newList)
+                                            }} className="float-right rounded-full justify-center items-center ml-2 bg-red-500 text-white">
+                                                <IoClose />
+                                            </button>
+                                            <div className="flex items-center justify-between">
+                                                <p className="font-bold">
+                                                    {medicine.nome}
+                                                </p>
+                                                <div className="flex items-center gap-2">
+                                                    <p>
+                                                        {medicine.quantity}
+                                                    </p>
+                                                    <p>
+                                                        {medicine.measure}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <p>
+                                                    {medicine.posologia}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                            <div className="text-sm flex flex-col items-center">
+                                <div className="w-3/5 mx-auto h-[0.5px] bg-zinc-700">
+
+                                </div>
+                                <div>
+                                    <p>Nome do dentista CRO 777777777</p>
+                                </div>
+                                <div>
+                                    {format(new Date(), "dd/MM/yyyy")}
+                                </div>
+
+                            </div>
                         </div>
                     </div>
                 )
